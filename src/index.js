78e8b5f4ec40beafd9c51cc4c2b62e77a2543b4c
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-10-03 23:24:26 +0300

    feature: #4 function incrementor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 // DO WHATEVER YOU WANT HERE
 
 // 1
-const createEnumerableProperty = (propertyName) => propertyName;
+const createEnumerableProperty = propertyName => propertyName;
 
 // 2
-const createNotEnumerableProperty = (propertyName) => Symbol(propertyName);
+const createNotEnumerableProperty = propertyName => Symbol(propertyName);
 
 // 3
-const createProtoMagicObject = () => createProtoMagicObject;
-createProtoMagicObject.prototype = Function.prototype;
-
-
-
+const createProtoMagicObject = () => {
+  createProtoMagicObject.prototype = Function.prototype;
+  return createProtoMagicObject;
+};
 
 // 4
-const incrementor = () => {};
+const incrementor = () => {
+  incrementor.counter = (incrementor.counter || 0) + 1;
+  const inner = () => incrementor();
+  inner.valueOf = () => incrementor.counter;
+  return inner;
+};
+
+
+
+
 
 // 5
 const asyncIncrementor = () => {};
@@ -38,5 +46,3 @@ exports.returnBackInSecond = returnBackInSecond;
 exports.getDeepPropertiesCount = getDeepPropertiesCount;
 exports.createSerializedObject = createSerializedObject;
 exports.sortByProto = sortByProto;
-
-
